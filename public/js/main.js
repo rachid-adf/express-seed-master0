bc03ca043f35ff562714f31738b212bf45e2a099
rachid-adf <rachid.benseba@adfdiesel.com>
2017-02-16 10:36:16 -0500

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+angular
+    .module('rachid', ['ui.grid'])
+    .controller('MainCtrl', function($scope){
+
+        $scope.myData = [
+            {
+                "firstName": "Cox",
+                "lastName": "Carney",
+                "company": "Enormo",
+                "employed": true
+            },
+            {
+                "firstName": "Lorraine",
+                "lastName": "Wise",
+                "company": "Comveyer",
+                "employed": false
+            },
+            {
+                "firstName": "Nancy",
+                "lastName": "Waters",
+                "company": "Fuelton",
+                "employed": false
+            }
+        ];
+
+    });
